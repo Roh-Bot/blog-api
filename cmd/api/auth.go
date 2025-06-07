99237d0b795993fa28052073ec044aa55be1934f
Rohit Devadiga <devadiga.rohit@gmail.com>
2025-06-07 15:24:25 +0530

    #fix  - Fixed compilation errors in auth.go

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -16,10 +16,10 @@ func (s *Server) authLoginUser(ctx *fiber.Ctx) error {
 	// Parse request body and get user details from user service
 	user := new(AuthLoginUser)
 	if err := ctx.BodyParser(user); err != nil {
-		return s.badRequest(ctx, err.Error())
+		return s.badRequest(ctx, err, err.Error())
 	}
 	if err := s.Validator.Struct(user); err != nil {
-		return s.badRequest(ctx, validationToErrorMessage(err))
+		return s.badRequest(ctx, err, validationToErrorMessage(err))
 	}
 
 	if isValid := s.Services.Auth.IsValid(user.Username); !isValid {
@@ -28,7 +28,7 @@ func (s *Server) authLoginUser(ctx *fiber.Ctx) error {
 
 	token, err := s.Services.Auth.GenerateToken(user.Username)
 	if err != nil {
-		return s.internalServerError(ctx, err.Error())
+		return s.internalServerError(ctx, err, err.Error())
 	}
 
 	response := map[string]any{
